b361c6a87282769ea3112638b11de992d50526d6
bilalnaseer7 <bn2168@nyu.edu>
2026-04-01 14:14:22 -0400

    Add retrieve_reviews function for querying reviews

--- src/retrieval.py
+++ src/retrieval.py
@@ -1,1 +1,10 @@
-
+def retrieve_reviews(query, df, top_k=5):
+    query = query.lower()
+    
+    results = []
+    
+    for text in df["text"]:
+        if any(word in text.lower() for word in query.split()):
+            results.append(text)
+    
+    return results[:top_k]
