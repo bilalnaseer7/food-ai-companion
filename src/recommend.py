80c6ca2479f19610b77b9fba1a2118c65a63011d
bilalnaseer7 <bn2168@nyu.edu>
2026-04-01 14:14:36 -0400

    Implement recommend function for restaurant suggestions
    Add a recommendation function that generates a prompt based on retrieved reviews.

--- src/recommend.py
+++ src/recommend.py
@@ -1,1 +1,18 @@
+from retrieval import retrieve_reviews
 
+def recommend(query, df):
+    retrieved = retrieve_reviews(query, df)
+    
+    context = "\n".join(retrieved)
+    
+    prompt = f"""
+User wants: {query}
+
+Here are similar reviews:
+{context}
+
+Recommend a restaurant or food choice based on this.
+Explain briefly.
+"""
+    
+    return prompt  # for now just return prompt
