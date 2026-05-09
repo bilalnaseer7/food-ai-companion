1288c5b8cf2b7ac07fe61df1d6d31e8a7f42e140
Owen Nie <owenn@Mac.lan>
2026-05-08 23:57:05 -0400

    update

--- app.py
+++ app.py
@@ -1564,6 +1564,7 @@ div:has(> [class*="st-key-drink_recipe_card_"]) {
 	    box-shadow: none !important;
 	    resize: none !important;
 	    padding-left: 3px !important;
+        padding-top: 2px !important;
 	    overflow: hidden !important;
 	}
 	[data-testid="stChatInput"] button {
